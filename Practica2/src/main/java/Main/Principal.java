fd7d3a7cc49cc8eedefe8d13638d4788f2a67c7d
KennySalazar <99755600+KennySalazar@users.noreply.github.com>
2022-06-21 00:44:05 -0600

    Add files via upload

---
+++ Practica2/src/main/java/Main/Principal.java
@@ -0,0 +1,24 @@
+
+package Main;
+
+import JFrameTienda.Tienda;
+import JFrameTienda.aaa;
+
+
+
+
+/**
+ *
+ * @author Kenny
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+       Tienda tienda = new Tienda();
+       aaa b = new aaa();
+       b.setVisible(true);
+       //tienda.setVisible(true);
+        
+    }
+    
+}
